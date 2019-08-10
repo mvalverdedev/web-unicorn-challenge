21c622128757de744b171479ff65c77a5a508ef0
mvalverde <miguel.valverde11@gmail.com>
2019-08-10 02:01:53 -0500

    Se agrega soporte para pantallas menores a 361px (telefono)

--- content/scripts/profile.js
+++ content/scripts/profile.js
@@ -39,14 +39,44 @@ $(document).ready(function () {
 
   const $footer = document.getElementById('footer');
   const $options = { threshold: 1 };
-
+  
   function callback(entries, observer) {
     if (entries[0].isIntersecting) {
       $iconLinkedIn.classList.add('zoom-icon');
     }
   };
-
+  
   const $footerObserver = new IntersectionObserver(callback, $options);
   $footerObserver.observe($footer);
+  
+  const $screemMobile = window.matchMedia('(max-width: 360.98px)');
+  const $homeImg = document.getElementById('homeImg');
+  const $portfolioItem = document.getElementsByClassName('portfolio-item');
+  const $collapseItems = document.getElementById('collapseItems');
+  
+  validation($screemMobile);
+
+  $screemMobile.addListener(validation);
+  
+  function validation(event) {
+    if (event.matches) {
+      $homeImg.classList.replace('d-flex', 'd-none');
+
+      Array.prototype.forEach.call($portfolioItem, function(item) {
+          item.classList.replace('col-5', 'col-11');
+      });
+
+      $collapseItems.classList.add('m-2');
+      
+    }else{
+      $homeImg.classList.replace('d-none', 'd-flex');
+      
+      Array.prototype.forEach.call($portfolioItem, function(item) {
+        item.classList.replace('col-11', 'col-5');
+      });
+
+      $collapseItems.classList.remove('m-2');
+    }
+  }
 
 });
